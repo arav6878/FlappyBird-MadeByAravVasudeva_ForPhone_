294beaad40f5cd4f3fb9136d37e033b773e83c37
arav6878 <98506744+arav6878@users.noreply.github.com>
2023-07-16 12:43:26 +0530

    Update game.js

--- game.js
+++ game.js
@@ -5,10 +5,9 @@ const ctx = canvas.getContext("2d");
 const gameContainer = document.getElementById('game-container');
 
 const flappyImg = new Image();
-flappyImg.src = '/assets/flappy_dunk.png';
+flappyImg.src = 'assets/flappy_dunk.png';
 
-//Game constants
-const FLAP_SPEED = -3; //Improves the fluidity and accuracy of the bird when moving around the screen
+const FLAP_SPEED = -3; 
 const BIRD_WIDTH = 40;
 const BIRD_HEIGHT = 30;
 const PIPE_WIDTH = 50;
